e1b99a5d9eae7d1fdfea51c6356d1aafafa1f87a
mjp66117 <133701667+mjp66117@users.noreply.github.com>
2026-05-22 15:56:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,39 @@ WEATHER_API = os.getenv("WEATHER_API")
 
 bot = Bot(token=TOKEN)
 
-def weather_comment(temp, desc):
-    desc = desc.lower()
+def send_weather():
+    try:
+        url = f"https://api.openweathermap.org/data/2.5/weather?q=Baku&appid={WEATHER_API}&units=metric"
+        r = requests.get(url).json()
 
-    if "rain" in desc:
-        return "🌧 امروز بارونیه، چتر یادت نره"
-    elif "cloud" in desc:
-        return "☁️ آسمون کمی ابریه"
-    elif temp >= 30:
-        return "🔥 هوا خیلی گرمه"
-    elif temp >= 20:
-        return "🌊 هوا عالیه برای بیرون رفتن"
-    else:
-        return "🧥 هوا خنکه"
+        temp = round(r["main"]["temp"])
+        humidity = r["main"]["humidity"]
+        wind = round(r["wind"]["speed"] * 3.6)
+        desc = r["weather"][0]["description"]
+
+        emoji = "🌤"
+
+        today_shamsi = jdatetime.datetime.now().strftime("%Y/%m/%d")
+        today_miladi = datetime.now().strftime("%Y/%m/%d")
+
+        message = f"""
+{emoji} باکوچی | وضعیت هوای باکو
+
+📅 شمسی: {today_shamsi}
+📆 میلادی: {today_miladi}
+
+🌡 دما: {temp}°C
+💨 باد: {wind} km/h
+💧 رطوبت: {humidity}%
+🌦 وضعیت: {desc}
+"""
+
+        bot.send_message(chat_id=CHAT_ID, text=message)
+
+        print("MESSAGE SENT")
+
+    except Exception as e:
+        print("ERROR:", e)
 
 temp = round(r["main"]["temp"])
 humidity = r["main"]["humidity"]
