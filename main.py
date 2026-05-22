173a7806dad35c26d4c6151148991992aadff9b4
mjp66117 <133701667+mjp66117@users.noreply.github.com>
2026-05-22 16:17:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("ONLY THIS FILE RUNNING")
+import os
+import requests
+from time import sleep
+
+TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+print("BOT IS RUNNING")
+
+def send_test():
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": "✅ بات باکوچی آنلاین شد"
+    }
+
+    r = requests.post(url, data=data)
+
+    print(r.text)
+
+while True:
+    send_test()
+    sleep(60)
