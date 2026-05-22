06d8efea93056dcc99728771025edfc38331dc19
mjp66117 <133701667+mjp66117@users.noreply.github.com>
2026-05-22 16:18:50 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,69 @@
 import os
 import requests
+import jdatetime
 from time import sleep
+from datetime import datetime
 
 TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-print("BOT IS RUNNING")
+print("BAKUCHI WEATHER BOT RUNNING")
 
-def send_test():
+def send_weather():
 
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    try:
 
-    data = {
-        "chat_id": CHAT_ID,
-        "text": "✅ بات باکوچی آنلاین شد"
-    }
+        weather_data = requests.get("https://wttr.in/Baku?format=j1").json()
 
-    r = requests.post(url, data=data)
+        current = weather_data["current_condition"][0]
 
-    print(r.text)
+        temp = current["temp_C"]
+        humidity = current["humidity"]
+        wind = current["windspeedKmph"]
+        desc = current["weatherDesc"][0]["value"]
+
+        emoji = "🌤"
+
+        if "rain" in desc.lower():
+            emoji = "🌧"
+        elif "cloud" in desc.lower():
+            emoji = "☁️"
+        elif "sun" in desc.lower():
+            emoji = "☀️"
+
+        today_shamsi = jdatetime.datetime.now().strftime("%Y/%m/%d")
+        today_miladi = datetime.now().strftime("%Y/%m/%d")
+
+        message = f"""
+{emoji} باکوچی | وضعیت هوای باکو
+
+📅 شمسی: {today_shamsi}
+📆 میلادی: {today_miladi}
+
+🌡 دما: {temp}°C
+💨 باد: {wind} km/h
+💧 رطوبت: {humidity}%
+🌦 وضعیت: {desc}
+
+💬 باکو امروز یه حال و هوای خاص داره 😄
+
+@bakuchi_official_channel
+"""
+
+        url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+        data = {
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+
+        r = requests.post(url, data=data)
+
+        print(r.text)
+
+    except Exception as e:
+        print("ERROR:", e)
 
 while True:
-    send_test()
-    sleep(60)
+    send_weather()
+    sleep(3600)
